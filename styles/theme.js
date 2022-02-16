95af7994c47d919d31f0b589d28616921c5a3535
Javier Dominguez II <Javi7057@gmail.com>
2022-02-16 17:47:23 -0600

    initial setup for mui and next

---
+++ styles/theme.js
@@ -0,0 +1,15 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { deepPurple, amber } from "@mui/material/colors";
+
+// Create a theme instance.
+let theme = createTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: amber,
+    mode: 'dark'
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
