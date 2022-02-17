a1c89aa717d04597cfbce44b7c682b943a1e2c76
Javier Dominguez II <Javi7057@gmail.com>
2022-02-16 18:55:40 -0600

    added 3 new files in pages directory

---
+++ pages/contact.js
@@ -0,0 +1,5 @@
+const ContactPage = () => {
+  return <h1>This is the Contact Page</h1>
+};
+
+export default ContactPage;
---
+++ pages/menu.js
@@ -0,0 +1,7 @@
+const MenuPage = () => {
+  return (
+      <h1>This is the Menu Page</h1>
+  )
+};
+
+export default MenuPage;
