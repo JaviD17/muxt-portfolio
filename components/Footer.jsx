fc7677b4fae08f1c12641792a04d3c3b7353a4fe
Javier Dominguez II <Javi7057@gmail.com>
2022-02-28 21:29:26 -0600

    added Footer.jsx to _app.js and added facebook, instagram, and twitter links

---
+++ components/Footer.jsx
@@ -0,0 +1,48 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import MuiNextLink from "../components/MuiNextLink";
+
+const Footer = () => {
+  return (
+    <Box component="footer" sx={{ py: 5, bgcolor: "primary.main" }}>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        spacing={4}
+        sx={{ mb: 5 }}
+      >
+        <MuiNextLink
+          sx={{ textDecoration: "none", color: "common.white" }}
+          href="https://google.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Facebook fontSize="large" />
+        </MuiNextLink>
+        <MuiNextLink
+          sx={{ textDecoration: "none", color: "common.black" }}
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Instagram fontSize="large" />
+        </MuiNextLink>
+        <MuiNextLink
+          sx={{ textDecoration: "none", color: "secondary.main" }}
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Twitter fontSize="large" />
+        </MuiNextLink>
+      </Stack>
+      <Typography align="center" color="common.white">
+        © {new Date().getFullYear()}, Javi's Portfolio
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import createEmotionCache from '../styles/createEmotionCache';
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../styles/createEmotionCache";
 
 import "/styles/globals.css";
-import theme from '../styles/theme';
+import theme from "../styles/theme";
 import Header from "../components/Header";
-
+import Footer from "../components/Footer";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -26,6 +26,7 @@ export default function MyApp(props) {
         <CssBaseline />
         <Header />
         <Component {...pageProps} />
+        <Footer />
       </ThemeProvider>
     </CacheProvider>
   );
